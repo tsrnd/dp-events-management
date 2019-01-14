dcd8781e797d8c7ca1c5f4009c0e5180d93a765b
at-nhannguyen2 <nhan.nguyen2@asiantech.vn>
2019-01-14 19:10:32 +0700

    create structer project

--- myproject/settings.py
+++ myproject/settings.py
@@ -35,7 +35,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin', 'django.contrib.auth',
     'django.contrib.contenttypes', 'django.contrib.sessions',
-    'django.contrib.messages', 'django.contrib.staticfiles', 'myapp'
+    'django.contrib.messages', 'django.contrib.staticfiles', 'myapp',
+    'comments', 'events', 'notifications'
 ]
 
 MIDDLEWARE = [
@@ -151,3 +152,20 @@ STATIC_URL = '/static/'
 #    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 #    'PAGE_SIZE': 10
 #}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
