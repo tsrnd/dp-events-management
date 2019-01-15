0acdc3b2f6ca4327bc05297b38f8346c2f747a1b
Nguyen Van Phuc <phuc.nguyen3@asiantech.vn>
2019-01-15 14:54:12 +0700

    update model events comment

--- myapp/comments/models.py
+++ myapp/comments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from events.models import Event
 
-# Create your models here.
+
+class EventComments(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+    comment_content = models.TextField()
+    is_delete = models.BooleanField(default=False)
+    root_comment = models.IntegerField(null=True)
+
+    class Meta:
+        db_table = 'tbl_events_comment'
