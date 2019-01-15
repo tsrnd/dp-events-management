2e08fd37eef821d7e533dd22f87c8fab964e0600
at-loctang <loc.tang@asiantech.vn>
2019-01-15 16:08:29 +0700

    add

--- myproject/settings.py
+++ myproject/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
 {
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': ['templates'],
+    'DIRS': ['myui'],
     'APP_DIRS': True,
     'OPTIONS': {
         'context_processors': [
