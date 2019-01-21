1a8fd0cfde591dcd2e0f92772edea6fefde3940e
at-nhannguyen2 <nhan.nguyen2@asiantech.vn>
2019-01-21 09:41:48 +0700

    add templates login, register

--- myui/views.py
+++ myui/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home(request):
     context = {}
     return render(request, 'events/index.html', context)
+
+
+def signin(request):
+    context = {}
+    return render(request, 'users/signin.html', context)
+
+
+def signup(request):
+    context = {}
+    return render(request, 'users/signup.html', context)
