f4ee2c03a1ecdc14e0da4b4db2ee1d08e93c65e7
Nghia Vo V <nghia.vo@asiantech.vn>
2019-01-28 16:07:39 +0700

    edit url

--- myapp/events/urls.py
+++ myapp/events/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.EventView.as_view()),
     path('<int:id_event>/', views.event_detail, name='detail'),
     path('<int:id_event>/users/', views.getUsers, name='getusers'),
 ]
